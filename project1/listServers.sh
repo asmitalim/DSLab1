5554841f8ce7bec8135a24b4b812ec268a48b79f
“asmital” <limaye.asmita@gmail.com>
2023-10-05 21:25:46 +0530

    listserver and killservers is updated ps -eo user,pid,cmd

--- project1/listServers.sh
+++ project1/listServers.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-ps -eo pid,cmd | grep server.py | grep -v grep | sed "s/ \+/ /g" | cut -d' ' -f2,6 | awk '{print "Server-" $2 " ,PID: " $1}'
-ps -eo pid,cmd | grep client.py | grep -v grep | sed "s/ \+/ /g" | cut -d' ' -f2,6 | awk '{print "Client-" $2 " ,PID: " $1}'
-ps -eo pid,cmd | grep frontend.py | grep -v grep | sed "s/ \+/ /g" | cut -d' ' -f2,6 | awk '{print "Frontend" $2 " ,PID: " $1}'
+ps -eo user,pid,cmd | grep server.py | grep -v grep | sed "s/ \+/ /g" | cut -d' ' -f2,6 | awk '{print "Server-" $2 " ,PID: " $1}'
+ps -eo user,pid,cmd | grep client.py | grep -v grep | sed "s/ \+/ /g" | cut -d' ' -f2,6 | awk '{print "Client-" $2 " ,PID: " $1}'
+ps -eo user,pid,cmd | grep frontend.py | grep -v grep | sed "s/ \+/ /g" | cut -d' ' -f2,6 | awk '{print "Frontend" $2 " ,PID: " $1}'
